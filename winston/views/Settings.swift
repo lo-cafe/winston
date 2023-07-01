90a155a5db82dd00d1dbfd86bc29c17d753b512b
Igor Marcossi <igormarcossi@hey.com>
2023-07-01 06:55:23 -0300

    Nice

--- winston/views/Settings.swift
+++ winston/views/Settings.swift
@@ -6,15 +6,49 @@
 //
 
 import SwiftUI
+import Defaults
 
 struct Settings: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  @Environment(\.openURL) var openURL
+  @Default(.preferredSort) var preferredSort
+  @EnvironmentObject var redditAPI: RedditAPI
+  var body: some View {
+    GoodNavigator {
+      VStack {
+        List {
+          Section {
+            Picker("Default posts sorting", selection: $preferredSort) {
+              ForEach(SubListingSortOption.allCases, id: \.self) { val in
+                HStack(spacing: 8) {
+                  Image(systemName: val.rawVal.icon)
+                  Text(val.rawVal.id.capitalized)
+                }
+                .fixedSize()
+              }
+            }
+          }
+        }
+                if let refreshToken = redditAPI.loggedUser.refreshToken {
+                  Text(refreshToken)
+                  Button("Logout") {
+                    redditAPI.loggedUser.accessToken = nil
+                    redditAPI.loggedUser.refreshToken = nil
+                    redditAPI.loggedUser.expiration = nil
+                    redditAPI.loggedUser.lastRefresh = nil
+                  }
+                } else {
+                  Button("Open auth") {
+                    openURL(redditAPI.getAuthorizationCodeURL())
+                  }
+                }
+      }
+      .navigationTitle("Settings")
     }
+  }
 }
 
-struct Settings_Previews: PreviewProvider {
-    static var previews: some View {
-        Settings()
-    }
-}
+//struct Settings_Previews: PreviewProvider {
+//  static var previews: some View {
+//    Settings()
+//  }
+//}
