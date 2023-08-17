8b4b8152cb421e12831d9f60ea25dfc7cf7ffc4c
Frostplexx <daniel.inama02@gmail.com>
2023-08-17 13:56:08 +0200

    added more wip compact mode options

---
+++ winston/views/Settings/views/GeneralPanel.swift
@@ -0,0 +1,18 @@
+//
+//  GeneralPanel.swift
+//  winston
+//
+//  Created by Daniel Inama on 17/08/23.
+//
+
+import SwiftUI
+
+struct GeneralPanel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GeneralPanel()
+}
