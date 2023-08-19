68556f4f1742f1619e6be6eadd03d2577b815d7a
Frostplexx <daniel.inama02@gmail.com>
2023-08-19 12:11:36 +0200

    added more options to compact mode, changed some more settings to titlecase, fixed delete cache button

---
+++ winston/views/Settings/subSettingView/postFontSettings.swift
@@ -0,0 +1,18 @@
+//
+//  postFontSettings.swift
+//  winston
+//
+//  Created by Daniel Inama on 19/08/23.
+//
+
+import SwiftUI
+
+struct postFontSettings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    postFontSettings()
+}
