e1a370f4090dacbe0a276b14b5848153340fefb2
Igor Marcossi <igormarcossi@hey.com>
2023-08-18 21:35:24 -0300

    Attempts to fix video crashes again

--- winston/components/VideoPlayerPost.swift
+++ winston/components/VideoPlayerPost.swift
@@ -23,7 +23,6 @@ struct VideoPlayerPost: View {
   @StateObject var sharedVideo: SharedVideo
   @Default(.preferenceShowPostsCards) var preferenceShowPostsCards
   @Default(.maxPostLinkImageHeightPercentage) var maxPostLinkImageHeightPercentage
-  @State private var fullscreen = false
   @State private var initialized = false
   @State var willBeginFullScreenPresentation: Bool = false
   @Default(.postLinksInnerHPadding) private var postLinksInnerHPadding
@@ -42,20 +41,14 @@ struct VideoPlayerPost: View {
     let propHeight = (contentWidth * sourceHeight) / sourceWidth
     let finalHeight = maxPostLinkImageHeightPercentage != 110 ? Double(min(maxHeight, propHeight)) : Double(propHeight)
     
-    AVPlayerControllerRepresentable(showFullScreen: $fullscreen, player: sharedVideo.player, aspect: .resizeAspectFill)
+    AVPlayerControllerRepresentable(autoPlayVideos: autoPlayVideos, player: sharedVideo.player, aspect: .resizeAspectFill)
       .shadow(radius: 0)
       .ignoresSafeArea()
-      .allowsHitTesting(fullscreen)
       .frame(width: contentWidth, height: CGFloat(finalHeight))
       .mask(RR(12, .black))
       .gesture(TapGesture())
       .contentShape(Rectangle())
       .overlay(sharedVideo.player.isPlaying ? nil : Image(systemName: "play.fill").foregroundColor(.white.opacity(0.75)).fontSize(32).shadow(color: .black.opacity(0.45), radius: 12, y: 8).allowsHitTesting(false))
-      .onTapGesture {
-        withAnimation {
-          fullscreen = true
-        }
-      }
       .onAppear {
         if autoPlayVideos && !initialized {
           sharedVideo.player.play()
@@ -68,46 +61,28 @@ struct VideoPlayerPost: View {
 }
 
 struct AVPlayerControllerRepresentable: UIViewControllerRepresentable {
-  @Default(.autoPlayVideos) private var autoPlayVideos
-  @Binding var showFullScreen: Bool
-  @State private var wasFullScreen: Bool = false
+  var autoPlayVideos: Bool
   let player: AVPlayer
   let aspect: AVLayerVideoGravity
   
   func makeUIViewController(context: Context) -> UIViewController {
     let controller = UIViewController()
-    let playerController = AVPlayerViewController()
+    let playerController = NiceAVPlayer(autoPlayVideos: autoPlayVideos)
+    playerController.allowsVideoFrameAnalysis = false
     playerController.player = player
     playerController.videoGravity = aspect
-    playerController.delegate = context.coordinator
     
     controller.addChild(playerController)
     controller.view.addSubview(playerController.view)
     playerController.didMove(toParent: controller)
-
+    
     return controller
   }
   
   func updateUIViewController(_ controller: UIViewController, context content: Context) {
-    if let playerController = controller.children[0] as? AVPlayerViewController {
-      if wasFullScreen != showFullScreen {
-        DispatchQueue.main.async {
-          withAnimation {
-            wasFullScreen = showFullScreen
-          }
-          if showFullScreen {
-            playerController.enterFullScreen(animated: true)
-          } else {
-            playerController.exitFullScreen(animated: true)
-          }
-        }
-      }
-    }
-  }
-  
-  func willDismiss() {
-    withAnimation {
-      showFullScreen = false
+    
+    if let playerController = controller.children[0] as? NiceAVPlayer {
+      playerController.autoPlayVideos = autoPlayVideos
     }
   }
   
@@ -122,48 +97,44 @@ struct AVPlayerControllerRepresentable: UIViewControllerRepresentable {
       self.parent = parent
     }
     
-    public func playerViewController(
-      _ playerViewController: AVPlayerViewController,
-      willBeginFullScreenPresentationWithAnimationCoordinator coordinator: UIViewControllerTransitionCoordinator
-    ) {
-      let isPlaying = playerViewController.player?.isPlaying ?? false
-      coordinator.animate(alongsideTransition: nil) { context in
-        // Add coordinated animations
-        if context.isCancelled {
-          // Still embedded inline
-        } else {
-          // Presented full screen
-          // Take strong reference to playerViewController if needed
-            self.parent.player.volume = 1.0
-            playerViewController.player?.play()
-        }
-      }
-    }
     
-    func playerViewController(
-      _ playerViewController: AVPlayerViewController,
-      willEndFullScreenPresentationWithAnimationCoordinator coordinator: UIViewControllerTransitionCoordinator
-    ) {
-      let isPlaying = parent.player.isPlaying
-      coordinator.animate { _ in
-        self.parent.player.volume = 0.0
-        self.parent.willDismiss()
-      }
-      coordinator.animate(alongsideTransition: nil) { context in
-        // Add coordinated animations
-        if context.isCancelled {
-            // Still full screen
-        } else {
-            // Embedded inline
-            // Remove strong reference to playerViewController if held
-          if !self.parent.autoPlayVideos { self.parent.player.pause() } else if isPlaying { self.parent.player.play() }
-        }
-      }
-    }
   }
 }
 
-extension AVPlayerViewController {
+class NiceAVPlayer: AVPlayerViewController, AVPlayerViewControllerDelegate {
+  var autoPlayVideos: Bool
+  override open var prefersStatusBarHidden: Bool {
+    return true
+  }
+  
+  init(autoPlayVideos: Bool) {
+    self.autoPlayVideos = autoPlayVideos
+    super.init(nibName: nil, bundle: nil)
+    self.delegate = self
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    self.autoPlayVideos = false
+    super.init(coder: aDecoder)
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    
+    showsPlaybackControls = false
+    
+    let tapGesture = UITapGestureRecognizer(target: self, action: #selector(didTapView))
+    self.view.addGestureRecognizer(tapGesture)
+  }
+  
+  override func viewDidDisappear(_ animated: Bool) {
+    super.viewDidDisappear(animated)
+  }
+  
+  @objc private func didTapView() {
+    enterFullScreen(animated: true)
+    showsPlaybackControls = true
+  }
   
   func enterFullScreen(animated: Bool) {
     let selector = NSSelectorFromString("enterFullScreenAnimated:completionHandler:")
@@ -175,10 +146,47 @@ extension AVPlayerViewController {
   
   func exitFullScreen(animated: Bool) {
     let selector = NSSelectorFromString("exitFullScreenAnimated:completionHandler:")
-
+    
     if self.responds(to: selector) {
       self.perform(selector, with: animated, with: nil)
     }
   }
   
+  func playerViewController(
+    _ playerViewController: AVPlayerViewController,
+    willBeginFullScreenPresentationWithAnimationCoordinator coordinator: UIViewControllerTransitionCoordinator
+  ) {
+    let isPlaying = playerViewController.player?.isPlaying ?? false
+    coordinator.animate(alongsideTransition: nil) { context in
+      if context.isCancelled {
+        // Still embedded inline
+      } else {
+        // Presented full screen
+        // Take strong reference to playerViewController if needed
+        self.player?.volume = 1.0
+        self.player?.play()
+        self.showsPlaybackControls = true
+      }
+    }
+  }
+  
+  func playerViewController(
+    _ playerViewController: AVPlayerViewController,
+    willEndFullScreenPresentationWithAnimationCoordinator coordinator: UIViewControllerTransitionCoordinator
+  ) {
+    let isPlaying = self.player?.isPlaying ?? false
+    coordinator.animate { _ in
+      self.player?.volume = 0.0
+      self.showsPlaybackControls = false
+    }
+    coordinator.animate(alongsideTransition: nil) { context in
+      if context.isCancelled {
+        // Still full screen
+      } else {
+        // Embedded inline
+        // Remove strong reference to playerViewController if held
+        if !self.autoPlayVideos { self.player?.pause() } else if isPlaying { self.player?.play() }
+      }
+    }
+  }
 }
