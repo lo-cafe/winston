e7bfe56c940f80dde18425e38dd70a357bc8c023
Frostplexx <daniel.inama02@gmail.com>
2023-08-25 15:00:21 +0200

    first draft

--- winston/components/URLImage.swift
+++ winston/components/URLImage.swift
@@ -9,6 +9,7 @@ import SwiftUI
 import NukeUI
 import Nuke
 import NukeExtensions
+import VisionKit
 
 struct URLImage: View {
   let url: URL
@@ -16,7 +17,7 @@ struct URLImage: View {
   var body: some View {
     LazyImage(url: url, transaction: Transaction(animation: .default)) { state in
       if let image = state.image {
-        image.resizable()
+        LiveTextInteraction(image: image.resizable())
       } else if state.error != nil {
         Color.red.opacity(0.1)
           .overlay(Image(systemName: "xmark.circle.fill").foregroundColor(.red))
@@ -25,5 +26,7 @@ struct URLImage: View {
       }
     }
     .processors(processors)
+    
+    
   }
 }
