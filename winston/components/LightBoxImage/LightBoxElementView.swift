919ae7cd06dad02763702635cd0d87e9d490b9fb
Frostplexx <daniel.inama02@gmail.com>
2023-08-28 11:08:02 +0200

    - added tap to hide overlay back in - added double tap to zoom back in - removed old code

--- winston/components/LightBoxImage/LightBoxElementView.swift
+++ winston/components/LightBoxImage/LightBoxElementView.swift
@@ -1,29 +1,32 @@
 //
-//  LightBoxElementView.swift
+//  LightBoxElementView2.swift
 //  winston
 //
-//  Created by Igor Marcossi on 07/08/23.
+//  Created by Daniel Inama on 26/08/23.
 //
 
 import SwiftUI
 import NukeUI
-
-struct LightBoxElement: Identifiable, Equatable {
-  var url: String
-  var size: CGSize
-  var id: String { self.url }
-}
+import Nuke
+import NukeExtensions
 
 struct LightBoxElementView: View {
   var el: MediaExtracted
   var onTap: (()->())?
   @Binding var isPinching: Bool
   var doLiveText: Bool = false
-  @State private var scale: CGFloat = 1.0
   @State private var anchor: UnitPoint = .zero
   @State private var offset: CGSize = .zero
   @State private var altSize: CGSize = .zero
   var body: some View {
+    ZoomableScrollView(onTap: onTap){
+      URLImage(url: el.url, doLiveText: doLiveText)
+    }
+    .frame(width: UIScreen.screenWidth) //TODO: .frame(maxWidth: .infinity, maxHeight: .infinity) Is the better approach than using the UIScreen
+    .preferredColorScheme(.dark)
+    .edgesIgnoringSafeArea(.all)
+    .statusBar(hidden: true)
+
     URLImage(url: el.url, doLiveText: doLiveText)
     .scaledToFit()
     .background(
