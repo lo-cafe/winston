f77842024054876875a61ae45e944578449c0fbe
Frostplexx <daniel.inama02@gmail.com>
2023-08-28 11:08:02 +0200

    - added tap to hide overlay back in - added double tap to zoom back in - removed old code

--- winston/components/LightBoxImage/LightBoxElementView.swift
+++ winston/components/LightBoxImage/LightBoxElementView.swift
@@ -1,31 +1,30 @@
 //
-//  LightBoxElementView.swift
+//  LightBoxElementView2.swift
 //  winston
 //
-//  Created by Igor Marcossi on 07/08/23.
+//  Created by Daniel Inama on 26/08/23.
 //
 
 import SwiftUI
 import NukeUI
-
-struct LightBoxElement: Identifiable, Equatable {
-  var url: String
-  var size: CGSize
-  var id: String { self.url }
-}
+import Nuke
+import NukeExtensions
 
 struct LightBoxElementView: View {
   var el: MediaExtracted
   var onTap: (()->())?
   @Binding var isPinching: Bool
   var doLiveText: Bool = false
-  @State private var scale: CGFloat = 1.0
   @State private var anchor: UnitPoint = .zero
   @State private var offset: CGSize = .zero
   var body: some View {
-    URLImage(url: el.url, doLiveText: doLiveText)
-    .scaledToFit()
-    .pinchToZoom(onTap: onTap, size: el.size, isPinching: $isPinching, scale: $scale, anchor: $anchor, offset: $offset)
-    .id(el.id)
+    ZoomableScrollView(onTap: onTap){
+      URLImage(url: el.url, doLiveText: doLiveText)
+    }
+    .frame(width: UIScreen.screenWidth) //TODO: .frame(maxWidth: .infinity, maxHeight: .infinity) Is the better approach than using the UIScreen
+    .preferredColorScheme(.dark)
+    .edgesIgnoringSafeArea(.all)
+    .statusBar(hidden: true)
+
   }
 }
--- winston/components/LightBoxImage/ZoomableImageView.swift
+++ winston/components/LightBoxImage/ZoomableImageView.swift
@@ -12,15 +12,18 @@ import SwiftUI
 //Source https://stackoverflow.com/a/64110231
 struct ZoomableScrollView<Content: View>: UIViewRepresentable {
   private var content: Content
-  private var onTap: (() -> ())?
+  private var onTap: (()->())?
+  
+  
   init(onTap: (()->())? = nil, @ViewBuilder content: () -> Content) {
     self.content = content()
+    //set up helper class
     self.onTap = onTap
-    
   }
-
-
-
+  
+  
+  
+  
   func makeUIView(context: Context) -> UIScrollView {
     // set up the UIScrollView
     let scrollView = UIScrollView()
@@ -28,8 +31,10 @@ struct ZoomableScrollView<Content: View>: UIViewRepresentable {
     scrollView.maximumZoomScale = 20
     scrollView.minimumZoomScale = 1
     scrollView.bouncesZoom = true
+    scrollView.showsVerticalScrollIndicator = false
+    scrollView.showsHorizontalScrollIndicator = false
     
-
+    
     // create a UIHostingController to hold our SwiftUI content
     let hostedView = context.coordinator.hostingController.view!
     hostedView.translatesAutoresizingMaskIntoConstraints = true
@@ -37,30 +42,63 @@ struct ZoomableScrollView<Content: View>: UIViewRepresentable {
     hostedView.frame = scrollView.bounds
     scrollView.addSubview(hostedView)
     
+    
+    let doubleTapGesture = UITapGestureRecognizer(target: context.coordinator, action: #selector(Coordinator.handleDoubleTap(_:)))
+    doubleTapGesture.numberOfTapsRequired = 2
+    scrollView.addGestureRecognizer(doubleTapGesture)
+    
+    let tapGesture = UITapGestureRecognizer(target: context.coordinator, action: #selector(Coordinator.handleTap(_:)))
+    tapGesture.numberOfTapsRequired = 1
+    tapGesture.require(toFail: doubleTapGesture)
+    scrollView.addGestureRecognizer(tapGesture)
+    
     return scrollView
   }
   
-  func makeCoordinator() -> Coordinator {
-    return Coordinator(hostingController: UIHostingController(rootView: self.content))
-  }
-
+  
   func updateUIView(_ uiView: UIScrollView, context: Context) {
     // update the hosting controller's SwiftUI content
     context.coordinator.hostingController.rootView = self.content
     assert(context.coordinator.hostingController.view.superview == uiView)
+    
   }
-
+  
+  func makeCoordinator() -> Coordinator {
+    return Coordinator(hostingController: UIHostingController(rootView: self.content), onTap: self.onTap)
+  }
+  
   // MARK: - Coordinator
-
+  
   class Coordinator: NSObject, UIScrollViewDelegate {
     var hostingController: UIHostingController<Content>
-
-    init(hostingController: UIHostingController<Content>) {
+    var onTap: (() -> ())?
+    
+    init(hostingController: UIHostingController<Content>, onTap: (() -> ())? = nil) {
       self.hostingController = hostingController
+      self.onTap = onTap
     }
-
+    
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
       return hostingController.view
     }
+    
+    @objc func handleTap(_ gestureRecognizer: UITapGestureRecognizer) {
+      onTap?()
+    }
+    
+    @objc func handleDoubleTap(_ gestureRecognizer: UITapGestureRecognizer) {
+      if let scrollView = gestureRecognizer.view as? UIScrollView {
+        if scrollView.zoomScale == 1 {
+          let tapLocation = gestureRecognizer.location(in: scrollView)
+          let zoomRect = CGRect(origin: tapLocation, size: CGSize(width: 100, height: 100))
+          scrollView.zoom(to: zoomRect, animated: true)
+        } else {
+          scrollView.setZoomScale(1, animated: true)
+        }
+      }
+    }
   }
+  
 }
+
+
