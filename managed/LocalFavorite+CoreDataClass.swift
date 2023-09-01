d9054be0a9df9ea47fe9e1ad9d3c07869ceb3003
Frostplexx <daniel.inama02@gmail.com>
2023-09-01 10:51:22 +0200

    added local favorites as CoreData

---
+++ managed/LocalFavorite+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  LocalFavorite+CoreDataClass.swift
+//  winston
+//
+//  Created by Daniel Inama on 01/09/23.
+//
+
+import Foundation
+import CoreData
+
+@objc(LocalFavorite)
+public class LocalFavorite: NSManagedObject {
+  
+}
+
