a3cb96feb6412c895b9959bf29473cd0a0434cb6
Ethan Bills <74191134+ejbills@users.noreply.github.com>
2023-09-04 11:28:34 -0700

    Revert "remove loader modifier"
    This reverts commit df720f1af35b3abfd62a57135acefa3521bcf7a7.

--- winston/views/SubredditPosts.swift
+++ winston/views/SubredditPosts.swift
@@ -123,7 +123,6 @@ struct SubredditPosts: View {
             }
 
           }
-          
           if !lastPostAfter.isNil {
             ProgressView()
               .progressViewStyle(.circular)
@@ -139,6 +138,7 @@ struct SubredditPosts: View {
       .listStyle(.plain)
       .environment(\.defaultMinListRowHeight, 1)
     }
+    .loader(loading && posts.count == 0)
     .overlay(
       feedsAndSuch.contains(subreddit.id)
       ? nil
