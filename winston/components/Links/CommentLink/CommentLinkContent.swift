1b1303147b6948742985db7e59d55a504c94fa7d
Frostplexx <daniel.inama02@gmail.com>
2023-09-05 17:58:21 +0200

    some more fixes, the text still overflows the comments

--- winston/components/Links/CommentLink/CommentLinkContent.swift
+++ winston/components/Links/CommentLink/CommentLinkContent.swift
@@ -94,7 +94,7 @@ struct CommentLinkContent: View {
           HStack(spacing: 8) {
             if let author = data.author, let created = data.created {
               Badge(usernameColor: (post?.data?.author ?? "") == author ? opUsernameColor : (customCommentUsernameColor ? commentUsernameColor : Color.primary), showAvatar: preferenceShowCommentsAvatars, author: author, fullname: data.author_fullname, created: created, avatarURL: avatarsURL?[data.author_fullname!], stickied: data.stickied ?? false, locked: data.locked ?? false)
-
+              
             }
             
             Spacer()
@@ -214,8 +214,7 @@ struct CommentLinkContent: View {
                       .fontSize(15)
                       .lineLimit(lineLimit)
                   } else {
-                    MD(data.winstonBodyAttrEncoded.isNil ? .str(body) : .json(data.winstonBodyAttrEncoded!), fontSize: commentLinkBodySize)
-                      .frame(minWidth: 100, maxWidth: .infinity, minHeight: 100, maxHeight: .infinity)
+                    MD2(data.winstonBodyAttrEncoded.isNil ? .str(body) : .json(data.winstonBodyAttrEncoded!), fontSize: commentLinkBodySize)
                       .fixedSize(horizontal: false, vertical: true)
                       .overlay(
                         !selectable
@@ -224,8 +223,8 @@ struct CommentLinkContent: View {
                           .frame(width: sizer.size.width, height: sizer.size.height, alignment: .topLeading)
                           .background(Rectangle().fill(Color.listBG))
                       )
-
                   }
+                  
                 }
               }
               //              .padding(.leading, 6)
--- winston/components/MD2.swift
+++ winston/components/MD2.swift
@@ -12,27 +12,36 @@ import BetterSafariView
 import SafariServices
 
 struct MD2: UIViewRepresentable {
-  var attributedString: NSAttributedString
+  var attributedString: AttributedString
+  var str: String?
   
-  init(str: String, fontSize: CGFloat = 15) {
-    let document = Document(parsing: str)
-    var markdownosaur = Markdownosaur(baseFontSize: fontSize)
-    let attributedString = markdownosaur.attributedString(from: document)
-    self.attributedString = attributedString
+  init(_ content: MDType, fontSize: CGFloat = 15) {
+    switch content {
+    case .attr(let attr):
+      self.attributedString = attr
+    case .str(let str):
+      self.str = str
+      self.attributedString = stringToAttr(str, fontSize: fontSize)
+    case .json(let json):
+      let decoder = JSONDecoder()
+      let jsonData = (try? decoder.decode(AttributedString.self, from: json.data(using: .utf8)!)) ?? AttributedString()
+      self.attributedString = jsonData
+    }
   }
   
   func makeUIView(context: Context) -> UITextView {
     let textView = UITextView()
     textView.isEditable = false
     textView.isSelectable = true
-    textView.isScrollEnabled = true
-    textView.attributedText = attributedString
+    textView.isScrollEnabled = false
+    textView.sizeToFit()
+    textView.attributedText = NSAttributedString(attributedString)
     textView.delegate = context.coordinator // Set the delegate
     return textView
   }
   
   func updateUIView(_ uiView: UITextView, context: Context) {
-    uiView.attributedText = attributedString
+    uiView.attributedText = NSAttributedString(attributedString)
   }
   
   // Coordinator to handle the delegate method
@@ -43,6 +52,8 @@ struct MD2: UIViewRepresentable {
   // Coordinator class to handle delegate methods
   class Coordinator: NSObject, UITextViewDelegate {
     var parent: MD2
+    @EnvironmentObject private var routerProxy: RouterProxy
+    
     
     init(_ parent: MD2) {
       self.parent = parent
@@ -52,11 +63,27 @@ struct MD2: UIViewRepresentable {
     func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
       // Handle URL interaction here
       // You can open the URL, display it differently, or perform any other action as needed
-      print("Interacted with URL: \(URL.absoluteString)")
-      var vc = SFSafariViewController(url: URL)
-      UIApplication.shared.windows.first?.rootViewController?.present(vc, animated: true)
+//      print("Interacted with URL: \(URL.absoluteString)")
+      var vc: SFSafariViewController?
+      
+      switch interaction {
+      case .invokeDefaultAction:
+        vc = SFSafariViewController(url: URL)
+      case .presentActions:
+        break
+      case .preview:
+        break
+      @unknown default:
+        break
+      }
+      
       // Return false to allow the default action to be performed (e.g., open the URL)
-      return false
+      if let vc {
+        UIApplication.shared.windows.first?.rootViewController?.present(vc, animated: true)
+        return false
+      } else {
+        return true
+      }
     }
   }
 }
