a5d7bda53a702a4b205a4d698b13b91522f93f0d
sefidel <contact@sefidel.net>
2023-12-31 14:04:38 +0900

    support old.reddit links on winston-everywhere

--- winston-everywhere/src/openWinstonEverywhere.js
+++ winston-everywhere/src/openWinstonEverywhere.js
@@ -3,7 +3,7 @@ document.querySelectorAll('a').forEach(link => {
     try {
       var url = new URL(link.href);
       if (url.hostname.includes('reddit.com')) {
-        link.href = link.href.replace(/(https?:\/\/)?(www\.)?reddit\.com/gi, 'https://app.winston.cafe');
+        link.href = link.href.replace(/(https?:\/\/)?(www\.|old\.)?reddit\.com/gi, 'https://app.winston.cafe');
       }
     } catch (e) {
       console.error('Invalid URL', link.href);
