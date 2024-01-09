65f347efd605778c9bc81ad958e9de31f34ec05f
Ethan Bills <74191134+ejbills@users.noreply.github.com>
2024-01-09 08:00:46 -0800

    remove more frames

--- winston/components/Links/PostLink/PostLinkNormal.swift
+++ winston/components/Links/PostLink/PostLinkNormal.swift
@@ -143,8 +143,6 @@ struct PostLinkNormal: View, Equatable, Identifiable {
       }
       .postLinkStyle(post: post, sub: sub, theme: theme, size: winstonData.postDimensions.size, secondary: secondary, openPost: openPost, readPostOnScroll: defSettings.readOnScroll, hideReadPosts: defSettings.hideOnRead)
       .swipyUI(onTap: openPost, actionsSet: defSettings.swipeActions, entity: post, secondary: secondary)
-      .frame(width: winstonData.postDimensions.size.width, height: winstonData.postDimensions.size.height)
-      .fixedSize()
     }
   }
 }
