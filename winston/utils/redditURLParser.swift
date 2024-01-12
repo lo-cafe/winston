34c646bb7d385d2594b92c9d94472fcbd013d754
rbertus2000 <91765399+rbertus2000@users.noreply.github.com>
2024-01-12 10:33:26 +0100

    fix navigation

--- winston/utils/redditURLParser.swift
+++ winston/utils/redditURLParser.swift
@@ -19,8 +19,7 @@ enum RedditURLType: Equatable, Hashable {
 func parseRedditURL(_ rawUrlString: String) -> RedditURLType {
   let urlString = rawUrlString
     .replacingOccurrences(of: "winstonapp://", with: "https://app.winston.cafe/")
-    .replacingOccurrences(of: "old.reddit.com/", with: "")
-    .replacingOccurrences(of: "reddit.com/", with: "")
+    .replacingOccurrences(of: #"(www\.|old\.)?reddit\.com\/"#, with: "", options: .regularExpression)
   guard let urlComponents = URLComponents(string: urlString) else {
     return .other(link: urlString)
   }
@@ -38,8 +37,10 @@ func parseRedditURL(_ rawUrlString: String) -> RedditURLType {
           return .comment(id: commentId, postID: postId, subreddit: subredditName)
         }
         return .post(id: postId, subreddit: subredditName)
-      }
-      return .subreddit(name: subredditName)
+      } else if pathComponents.count > 2 && pathComponents[2] == "wiki" {
+				return .other(link: urlString)
+			}
+				return .subreddit(name: subredditName)
       
     case "user", "u":
       let username = pathComponents[1]
