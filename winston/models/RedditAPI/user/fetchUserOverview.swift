9c71e572c70622a1fa33c37920a8f1a0c9440fcf
Igor Marcossi <igor@lo.cafe>
2024-02-14 10:55:44 -0300

    Indent

--- winston/models/RedditAPI/user/fetchUserOverview.swift
+++ winston/models/RedditAPI/user/fetchUserOverview.swift
@@ -10,38 +10,38 @@ import Alamofire
 
 extension RedditAPI {
   func fetchUserOverview(_ userName: String, _ dataTypeFilter: String? = nil, _ after: String? = nil) async -> ([Either<PostData, CommentData>]?, String?)? {
-      var endpoint: String
-
-      if let dataTypeFilter = dataTypeFilter, !dataTypeFilter.isEmpty {
-        if dataTypeFilter.lowercased() == "posts" {
-          endpoint = "\(RedditAPI.redditApiURLBase)/user/\(userName)/submitted.json"
-        } else if dataTypeFilter.lowercased() == "comments" {
-          endpoint = "\(RedditAPI.redditApiURLBase)/user/\(userName)/comments.json"
-        } else {
-          print("Invalid dataTypeFilter")
-          return nil
-        }
+    var endpoint: String
+    
+    if let dataTypeFilter = dataTypeFilter, !dataTypeFilter.isEmpty {
+      if dataTypeFilter.lowercased() == "posts" {
+        endpoint = "\(RedditAPI.redditApiURLBase)/user/\(userName)/submitted.json"
+      } else if dataTypeFilter.lowercased() == "comments" {
+        endpoint = "\(RedditAPI.redditApiURLBase)/user/\(userName)/comments.json"
       } else {
-        endpoint = "\(RedditAPI.redditApiURLBase)/user/\(userName)/overview.json"
-      }
-
-      var urlComponents = URLComponents(string: endpoint)!
-
-      if let after = after {
-        urlComponents.queryItems = [URLQueryItem(name: "after", value: after)]
-      }
-
-      guard let requestURL = urlComponents.url else {
-        print("Error: Unable to create a valid URL")
+        print("Invalid dataTypeFilter")
         return nil
       }
-
+    } else {
+      endpoint = "\(RedditAPI.redditApiURLBase)/user/\(userName)/overview.json"
+    }
+    
+    var urlComponents = URLComponents(string: endpoint)!
+    
+    if let after = after {
+      urlComponents.queryItems = [URLQueryItem(name: "after", value: after)]
+    }
+    
+    guard let requestURL = urlComponents.url else {
+      print("Error: Unable to create a valid URL")
+      return nil
+    }
+    
     switch await self.doRequest(requestURL.absoluteString, method: .get, decodable: Listing<Either<PostData, CommentData>>.self)  {
-      case .success(let data):
+    case .success(let data):
       return (data.data?.children?.map { $0.data }.compactMap { $0 }, data.data?.after)
-      case .failure(let error):
-        print(error)
-        return nil
-      }
+    case .failure(let error):
+      print(error)
+      return nil
+    }
   }
 }
