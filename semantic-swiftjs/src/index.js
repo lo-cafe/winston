6674ed37074da8a872aa099e36fb21c82f068b39
Igor Marcossi <igor@lo.cafe>
2024-02-23 07:46:16 -0300

    Progress

--- semantic-swiftjs/src/index.js
+++ semantic-swiftjs/src/index.js
@@ -1,72 +1,133 @@
-import semver from "semver";
-import xcode from "xcode";
-import fs from "fs";
+import semver from "semver"
+import xcode from "xcode"
+import fs from "fs"
 
-import executeGitCommand from "./executeGitCommand.js";
-import generateCommitReport from "./getCommitMessages.js";
-import config from "../settings.json" assert { type: 'json' };
+import executeGitCommand from "./executeGitCommand.js"
+import generateCommitReport from "./getCommitMessages.js"
+import processItems from "./gptChangelog.js"
+import config from "../settings.json" assert { type: "json" }
 
 const bundleIds = config.targets.reduce((acc, target) => {
   const targetBundleIds = config.branches.map((branch) => {
-    if (branch.label) {
-      return `${config.companyBundleId}.${target}.${branch.label}`;
+    if (branch.preReleaseLabel) {
+      return `${config.companyBundleId}.${target}.${branch.preReleaseLabel}`
     }
-    return `${config.companyBundleId}.${target}`;
-  });
-  return acc.concat(targetBundleIds);
-}, []);
+    return `${config.companyBundleId}.${target}`
+  })
+  return acc.concat(targetBundleIds)
+}, [])
 
 function removeQuotes(str) {
-  if (!str || typeof str !== "string") return "";
-  return str.replace(/^"|"$/g, "");
+  if (!str || typeof str !== "string") return ""
+  return str.replace(/^"|"$/g, "")
 }
 
 executeGitCommand("git branch --show-current", (_, currBranch) => {
-  const branchConfig = config.branches.find((b) => b.name === currBranch);
-  if (!branchConfig) {
-    return;
+  const branchConfig = config.branches.find((b) => b.name === currBranch)
+  if (!branchConfig || branchConfig.versionate === false) {
+    return
   }
-  const branchConfigIndex = config.branches.findIndex(
-    (b) => b.name === currBranch,
-  );
+  const branchConfigIndex = config.branches.findIndex((b) => b.name === currBranch)
+
+  const smallerBranchesLabels = config.branches
+    .slice(branchConfigIndex + 1)
+    // .filter((x) => x.versionate !== false)
+    .map((x) => (x.preReleaseLabel ? x.preReleaseLabel : ""))
 
   generateCommitReport(async (report) => {
     // /^(feat|fix|docs|style|refactor|test|chore)(?:\((.*)\))?:(.*)$/,
 
-    const projectPath = "../../winston.xcodeproj/project.pbxproj";
+    const projectPath = "../winston.xcodeproj/project.pbxproj"
 
-    const project = xcode.project(projectPath).parse(() => {
-      const config = project.pbxXCBuildConfigurationSection();
-      const versionsObj = {};
-      Object.keys(config).forEach((key) => {
+    const project = xcode.project(projectPath).parse(async () => {
+      const configSection = project.pbxXCBuildConfigurationSection()
+      const versionsObj = {}
+      Object.keys(configSection).forEach((key) => {
         const bundleId = removeQuotes(
-          config[key].buildSettings &&
-            config[key].buildSettings.PRODUCT_BUNDLE_IDENTIFIER,
-        );
+          configSection[key].buildSettings && configSection[key].buildSettings.PRODUCT_BUNDLE_IDENTIFIER
+        )
         if (bundleIds.includes(bundleId)) {
-          versionsObj[bundleId] = {
-            version: config[key].buildSettings.MARKETING_VERSION,
-            setVersion: (newVer) => {
-              config[key].buildSettings.MARKETING_VERSION = newVer;
-            },
-          };
-        }
-      });
+          const bundleIdSuffix = bundleId.split(".").pop()
+          let preReleaseLabel = bundleIdSuffix
 
-      const currentVersion = `${config.companyBundleId}.`
+          if (config.targets.includes(bundleIdSuffix)) {
+            preReleaseLabel = ""
+          }
+
+          versionsObj[bundleId] = {
+            preReleaseLabel,
+            version: removeQuotes(configSection[key].buildSettings.MARKETING_VERSION),
+            setVersion: (newVer) => {
+              configSection[key].buildSettings.MARKETING_VERSION = newVer
+            },
+          }
+        }
+      })
+
+      const allVersionsSorted = Object.values(versionsObj)
+        .filter((x) => x.preReleaseLabel == branchConfig.preReleaseLabel)
+        .map((x) => x.version)
+        .sort(semver.compare)
+      let currentVersion = allVersionsSorted[allVersionsSorted.length - 1]
+
+      const smallerVersionsConfigs = Object.values(versionsObj).filter((x) =>
+        smallerBranchesLabels.includes(x.preReleaseLabel)
+      )
+
+      let newVersion = currentVersion
 
       if (report.feat) {
-        // console.log(await processItems(report.feat));
+        newVersion = branchConfig.preReleaseLabel
+          ? semver.inc(currentVersion, "preminor", branchConfig.preReleaseLabel, false)
+          : semver.inc(currentVersion, "minor", false)
       } else if (report.fix || report.style) {
+        newVersion = branchConfig.preReleaseLabel
+          ? semver.inc(currentVersion, "prepatch", branchConfig.preReleaseLabel, false)
+          : semver.inc(currentVersion, "patch", false)
       }
 
-      versionsObj["lo.cafe.winston"].setVersion("1.2.3");
+      if (newVersion === currentVersion) return
 
-      fs.writeFileSync(projectPath, project.writeSync());
-      console.log("new project written");
-      // console.log(releaseScheme);
-    });
+      // console.log("asaq")
+      const formattedReport = await processItems(report.feat)
+      // console.log("asa")
+      // console.log(formattedReport)
+      // const formattedReport = report
+      // console.log(report)
 
-    // await processItems(report.feat)
-  });
-});
+      const currVer = Object.values(versionsObj).find(
+        (x) => x.preReleaseLabel == branchConfig.preReleaseLabel
+      )
+
+      if (currVer) {
+        currVer.setVersion(newVersion)
+      }
+
+      writeReportToChangelog(formattedReport, newVersion, branchConfig.preReleaseLabel)
+
+      smallerVersionsConfigs.forEach((x) => {
+        const smallerVersion = semver.inc(newVersion, "prerelease", x.preReleaseLabel, false)
+        x.setVersion(smallerVersion)
+        if (config.branches.find((y) => x.preReleaseLabel === y.preReleaseLabel).versionate !== false) {
+          writeReportToChangelog(formattedReport, smallerVersion, x.preReleaseLabel)
+        }
+      })
+
+      fs.writeFileSync(projectPath, project.writeSync())
+      console.log("new project written")
+    })
+  })
+})
+
+function writeReportToChangelog(report, newVersion, preReleaseLabel) {
+  const changelogFile = `./changelogs/${preReleaseLabel ? preReleaseLabel : "production"}.json`
+  const fileContent = fs.readFileSync(changelogFile)
+  const changelog = JSON.parse(fileContent)
+  const foundChangelogIndex = changelog.findIndex((x) => x.version === newVersion)
+  if (foundChangelogIndex) {
+    changelog[foundChangelogIndex] = { version: newVersion, report }
+  } else {
+    changelog.push({ version: newVersion, report })
+  }
+  fs.writeFileSync(changelogFile, JSON.stringify(changelog))
+}
