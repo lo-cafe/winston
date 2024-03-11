6e1d40e29cd86206b763d901e87fa9690aef7c6a
Igor Marcossi <igor@lo.cafe>
2024-03-11 03:51:45 -0300

    Restored forceSubsAsTags theming option

--- winston/views/Settings/views/theming/Editing/Components/BadgeSettings.swift
+++ winston/views/Settings/views/theming/Editing/Components/BadgeSettings.swift
@@ -28,10 +28,10 @@ struct BadgeSettings: View {
             .resetter($theme.spacing, defaultVal.spacing)
         }
         
-//        FakeSection(footer: "Currently subrredits show inline in a text accent-colored on the right of the author. With this option you can force it to always appear as a accent-colored tag instead.") {
-//          Toggle("Force subs as tag", isOn: $theme.forceSubsAsTags)
-//            .padding(.horizontal, 16)
-//        }
+        FakeSection(footer: "Currently subrredits show inline in a text accent-colored on the right of the author. With this option you can force it to always appear as a accent-colored tag instead.") {
+          Toggle("Force subs as tag", isOn: $theme.forceSubsAsTags)
+            .padding(.horizontal, 16)
+        }
         
         if (showSub) {
           FakeSection("Subreddit Color") {
