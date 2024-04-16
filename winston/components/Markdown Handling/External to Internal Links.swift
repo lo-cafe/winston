f47b5b101b70d87d775f92f519b88855bde47b3b
rbertus2000 <91765399+rbertus2000@users.noreply.github.com>
2024-04-16 20:06:50 +0200

    inline image option (#480)
    * inline image option
    
    * typo
    
    * fixes padding also it doesnt need to be optional.
    
    ---------
    
    Co-authored-by: Ethan Bills <74191134+ejbills@users.noreply.github.com>

--- winston/components/Markdown Handling/External to Internal Links.swift
+++ winston/components/Markdown Handling/External to Internal Links.swift
@@ -8,85 +8,92 @@
 import Foundation
 
 class MarkdownUtil {
-  static func containsSpoiler(_ text: String) -> Bool {
-    return text.contains("&gt;!") && text.contains("!&lt;") ||
-    text.contains(">!") && text.contains("!<")
-  }
-  
-  static func formatForMarkdown(_ text: String, showSpoiler: Bool = false) -> String {
-    var processedText = text
-    
-    // Replace http:// or https:// in existing markdown links
-    processedText = processedText.replacingOccurrences(
-      of: #"([[(\w\])\/.:*]+])\((https?:\/\/)(\S+?|)(?:)\)"#,
-      with: "$1(winstonapp://$3)",
-      options: .regularExpression
-    )
-
-    // Replace URLs with http:// or https:// (if not already in markdown format)
-    processedText = processedText.replacingOccurrences(
-      of: "\\b(?<!\\[)(https?://)(\\S+)(?!\\])\\b",
-      with: "[$0](winstonapp://$2)",
-      options: .regularExpression
-    )
-    
-    // Replace /u/example or u/example
-    processedText = processedText.replacingOccurrences(
-      of: "(\\s|^)(/?u/\\w+)(\\s|\\b)",
-      with: " [$2](winstonapp://$2) ",
-      options: [.regularExpression, .caseInsensitive]
-    )
-
-    // Replace /r/example or r/example
-    processedText = processedText.replacingOccurrences(
-      of: "(\\s|^)(/?r/\\w+)(\\s|\\b)",
-      with: " [$2](winstonapp://$2) ",
-      options: [.regularExpression, .caseInsensitive]
-    )
-		
-		// Replace &#x200B; and &nbsp; with a space
-		processedText = processedText.replacingOccurrences(
-			of: "&amp;#x200B;|&amp;nbsp;",
-			with: " ",
-			options: [.regularExpression, .caseInsensitive]
-		)
-    
-    processedText = processedText.replacingOccurrences(
-      of: "&gt;",
-      with: ">"
-    )
-    
-    processedText = processedText.replacingOccurrences(
-      of: "&lt;",
-      with: "<"
-    )
-    
-    processedText = processedText.replacingOccurrences(
-      of: "&Hat;",
-      with: "^"
-    )
-
-    if containsSpoiler(processedText) {
-      if showSpoiler {
-        processedText = processedText.replacingOccurrences(
-          of: ">!",
-          with: ""
-        )
-        
-        processedText = processedText.replacingOccurrences(
-          of: "!<",
-          with: ""
-        )
-      } else {
-        processedText = processedText.replacingOccurrences(
-          of: ">!(.*?)!<",
-          with: "■",
-          options: .regularExpression
-        )
-      }
-      
+    static func containsSpoiler(_ text: String) -> Bool {
+        return text.contains("&gt;!") && text.contains("!&lt;") ||
+        text.contains(">!") && text.contains("!<")
     }
+    
+    static func formatForMarkdown(_ text: String, showSpoiler: Bool = false) -> String {
+        var processedText = text
         
-    return processedText
-  }
+        // Replace http:// or https:// in existing markdown links
+        processedText = processedText.replacingOccurrences(
+            of: #"([[(\w\])\/.:*]+])\((https?:\/\/)(\S+?|)(?:)\)"#,
+            with: "$1(winstonapp://$3)",
+            options: .regularExpression
+        )
+        
+        // Replace URLs with http:// or https:// (if not already in markdown format)
+        processedText = processedText.replacingOccurrences(
+            of: #"\b(?<!\[)(https?:\/\/)(.*\.(?:png|jpe?g|bmp|tiff|webp|svgz?|ico)(?:\?.*)?)(?!\])\b"#,
+            with: "[![$0]($0)](winstonapp://$2)",
+            options: .regularExpression
+        )
+        
+        // Replace URLs with http:// or https:// (if not already in markdown format)
+        processedText = processedText.replacingOccurrences(
+            of: "\\b(?<!\\[|\\()(https?://)(\\S+)(?!\\]|\\))\\b",
+            with: "[$0](winstonapp://$2)",
+            options: .regularExpression
+        )
+        
+        // Replace /u/example or u/example
+        processedText = processedText.replacingOccurrences(
+            of: "(\\s|^)(/?u/\\w+)(\\s|\\b)",
+            with: " [$2](winstonapp://$2) ",
+            options: [.regularExpression, .caseInsensitive]
+        )
+        
+        // Replace /r/example or r/example
+        processedText = processedText.replacingOccurrences(
+            of: "(\\s|^)(/?r/\\w+)(\\s|\\b)",
+            with: " [$2](winstonapp://$2) ",
+            options: [.regularExpression, .caseInsensitive]
+        )
+        
+        // Replace &#x200B; and &nbsp; with a space
+        processedText = processedText.replacingOccurrences(
+            of: "&amp;#x200B;|&amp;nbsp;",
+            with: " ",
+            options: [.regularExpression, .caseInsensitive]
+        )
+        
+        processedText = processedText.replacingOccurrences(
+            of: "&gt;",
+            with: ">"
+        )
+        
+        processedText = processedText.replacingOccurrences(
+            of: "&lt;",
+            with: "<"
+        )
+        
+        processedText = processedText.replacingOccurrences(
+            of: "&Hat;",
+            with: "^"
+        )
+        
+        if containsSpoiler(processedText) {
+            if showSpoiler {
+                processedText = processedText.replacingOccurrences(
+                    of: ">!",
+                    with: ""
+                )
+                
+                processedText = processedText.replacingOccurrences(
+                    of: "!<",
+                    with: ""
+                )
+            } else {
+                processedText = processedText.replacingOccurrences(
+                    of: ">!(.*?)!<",
+                    with: "■",
+                    options: .regularExpression
+                )
+            }
+            
+        }
+        
+        return processedText
+    }
 }
